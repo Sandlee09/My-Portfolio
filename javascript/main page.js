290d0396a197a56a0aefa22fc08a7bccbe5b0161
Sandlee09 <sandlee09@outlook.com>
2017-09-04 20:17:57 -0300

    9/4/2017

--- javascript/main page.js
+++ javascript/main page.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
 	$(window).scroll(function() {    
 	    var scroll = $(window).scrollTop();
@@ -7,6 +8,45 @@ $(document).ready(function() {
 	    );
 	});
 
+	quote();
+
+
+$(window).scroll(function(){
+		if ($(this).scrollTop() > 400) {
+			$('#scroll_btn').fadeIn();
+		} else {
+			$('#scroll_btn').fadeOut();
+		}
+	});
 	
+	//Click event to scroll to top
+	$('#scroll_btn').click(function(){
+		$('html , body').animate({scrollTop : 0},800);
+		return false;
+	});
 
 });
+
+
+
+function quote() {
+
+	var random_num = Math.floor(Math.random(1) * 5);
+
+	switch(random_num) {
+		case 1 : $('#main_quote').append("\"Creativity is a habit, and the best creativity is the result of good work habits.\" - Twyla Tharp"); break;
+
+		case 2 : $('#main_quote').append("\"Anyone who has ever made anything of importance was disciplined.\" - Andrew Hendrixson"); break;
+
+		case 3 : $('#main_quote').append("\"Creativity is intelligence having fun.\" - Albert Einstein"); break;
+
+		case 4 : $('#main_quote').append("\"Genius is 1% inspiration, 99% perspiration.\" - Thomas Edison"); break;
+
+		case 5 : $('#main_quote').append("\"You must be the change you wish to see in the world.\" - Mahatma Gandhi"); break;
+
+		default : $('#main_quote').append("\"Do it with passion, or not at all.\" - Rosa Nouchette Carey");
+
+	}
+}
+
+
